cc92196e734a92cf6355fb3da4b6ec1863fc126d
Bharath-ram-prof <bharath.ram.prof@gmail.com>
2026-03-26 13:37:46 +0530

    Add files via upload

---
+++ render-api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is working successfully on Render" });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", server: "Render Live" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
